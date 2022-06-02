f90b880a5b2a8fcb8f7b9604315f37e01eee1519
Julio Santacruz <aram.jsh1@gmail.com>
2022-06-01 19:05:55 -0700

    portfolio v1.2 deploy

---
+++ main.js
@@ -0,0 +1,57 @@
+const menu = document.getElementById('navbar');
+const indicador = document.getElementById('indicador');
+const secciones = document.querySelectorAll('.seccion');
+
+
+
+// TAMANIO DE LA BARRA INDICADOR DEL MENU
+let indicadorSize = menu.querySelector('a').offsetWidth;
+// console.log(`el ancho del boton es de ${indicadorSize} px`)
+indicador.style.width = indicadorSize + 'px'
+indicador.style.transform = `translate(${indicadorSize}px)`
+
+
+let indexSeccionActiva;
+
+// OBSERVER (HACE TRACK A LA PAGINA PARA MOVER EL INDICADOR DE BOTON)
+const observer = new IntersectionObserver((entradas, observer) => {
+    entradas.forEach(entrada => {
+        // console.log(entrada.target)
+        if (entrada.isIntersecting) {
+            // console.log(entrada.target)
+            //OBTENEMOS LA SECCION QUE ESTA ENTRANDO A PANTALLA
+            //CREAMOS UN ARREGLE CON [...SECCIONES] Y USAMOS EL METODO INDEXOF
+            indexSeccionActiva = [...secciones].indexOf(entrada.target)
+
+
+            //MULTIPLICAMOS EL ANCHO DEL INDICADOR POR EL INDICE DE LA SECCION
+            indicador.style.transform = `translateX(${(indicadorSize * (indexSeccionActiva+1))}px)`
+        }
+    })
+
+}, {
+    rootMargin: '-80px 0px 0px 0px',
+    threshold: .2
+})
+
+// agregamos observer para el hero
+observer.observe(document.getElementById('welcome-section'))
+
+// ASIGNAMOS EL OBSERVADOR A CADA UNA DE LAS SECCIONES
+secciones.forEach(seccion => observer.observe(seccion))
+
+
+//EVENTO PARA CUANDO LA PANTALLA CABIE DE TAMANIO
+const onResize = () => {
+    //cuando la pantalla cambia de lugar el ancho del indicador se recalcula
+    indicadorSize = menu.querySelector('a').offsetWidth;
+
+    //cmbiamos el tamanio del indicador
+    indicador.style.width = indicadorSize + 'px'
+
+    //cambiamos la nueva posicion
+    indicador.style.transform = `translateX(${(indicadorSize * indexSeccionActiva)}px)`
+
+}
+
+window.addEventListener('resize', onResize)
